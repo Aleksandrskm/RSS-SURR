e0faa588fc00b6ee29644791a1376220e8ebf2c3
Aleksandrskm <aleksandr.skm@mail.ru>
2024-08-05 12:27:11 +0300

    Добавил модалку

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -603,3 +603,27 @@ btnStartSim.addEventListener('click',()=>{
   });
 });
 
+
+// Получаем модальное окно
+const modal = document.getElementById("myModal");
+// Получаем кнопку, которая открывает модальное окно
+const btn = document.getElementById("openModal");
+// Получаем элемент <span>, который закрывает модальное окно
+const span = document.getElementsByClassName("close")[0];
+//// Когда пользователь нажимает на кнопку, открываем модальное окно
+//btn.onclick = function() {
+//    modal.style.display = "block";
+//}
+//// Когда пользователь нажимает на <span> (x), закрываем модальное окно
+//span.onclick = function() {
+//    modal.style.display = "none";
+//}
+//// Когда пользователь кликает вне модального окна, закрываем его
+//window.onclick = function(event) {
+//    if (event.target == modal) {
+//        modal.style.display = "none";
+//    }
+//}
+
+btn.addEventListener("click", ()=>{modal.style.display = "flex"})
+span.addEventListener("click", ()=>{modal.style.display = "none"})
