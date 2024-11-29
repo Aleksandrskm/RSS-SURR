cee78786d9c947c1341a177d977241f0a09c3a30
Вячеслав <62754486+vbelyavcev@users.noreply.github.com>
2024-11-29 15:55:23 +0300

    подправил внешний вид сайта

--- website/views.py
+++ website/views.py
@@ -8,32 +8,10 @@ def index():
     return render_template('base.html')
 
 
-@views.route('/call__simulator')
-def function_call():
-    return render_template('call__simulator.html')
+@views.route('/plan_RSS')
+def function_plan_RSS():
+    return render_template('plan_RSS.html')
 
-
-@views.route('/azimuth_and_elevation_angle')
-def function_az():
-    return render_template('azimuth_and_elevation_angle.html')
-
-
-@views.route('/finding_coordinates')
-def function_find():
-    return render_template('finding_coordinates.html')
-
-@views.route('/call_flow_simulator')
-def function_call_flow():
-    return render_template('call_flow_simulator.html')
-
-@views.route('/get_coordinates')
-def function_get():
-    return render_template('get_coordinates.html')
-
-@views.route('/communication_availability')
-def function_com():
-    return render_template('communication_availability.html')
-
-@views.route('/viewing_tables')
-def function_view():
-    return render_template('viewing_tables.html')
+@views.route('/transmitting_antennas_RSS')
+def function_transmitting_antennas_RSS():
+    return render_template('transmitting_antennas_RSS.html')
